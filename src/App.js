426c32eb15a4012f0beb9aaeb02879916a2e4b4a
DevScissors <sean.scissors@gmail.com>
2018-05-11 23:34:18 -0700

    Added Ravenous initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
