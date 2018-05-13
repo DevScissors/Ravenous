2bc28f7da457e53f92bea73d13d5c0d1cd628fb6
DevScissors <sean.scissors@gmail.com>
2018-05-13 10:56:57 -0700

    Step 2 - Project Ravenous

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
 
-import SearchBar from './components/SearchBar/SearchBar.js';
-import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+import BusinessList from '../BusinessList/BusinessList.js';
 
-class App extends Component {
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90 
+}
+
+const businesses = [
+  business, 
+  business, 
+  business, 
+  business, 
+  business, 
+  business
+];
+
+class App extends React.Component {
   render() {
     return (
       <div class="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
